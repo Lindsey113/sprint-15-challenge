03dd17f32b478b1bb160b91d41ade7610c4f5d08
Lindsey McCluskey <lindsey_mccluskey@yahoo.ca>
2025-01-05 14:06:01 -0800

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -4,12 +4,12 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (!token) {
-    return next({ status: 401, message: "token required" })
+    return res.status(401).json({message: "token required"})
   }
 
   jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
     if (err) {
-      return next({ status: 401, message: "token invalid" })
+      return res.status(401).json({message: "token invalid"})
     } else {
       req.decodedToken = decodedToken;
       next()

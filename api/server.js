1fda4211d04b95bbd2f5e401b6832a815c12fa1b
Lindsey McCluskey <lindsey_mccluskey@yahoo.ca>
2025-01-05 14:29:45 -0800

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const usernameAvailability = require('./auth/auth-middleware.js')
 const restrict = require('./middleware/restricted.js');
 
 const authRouter = require('./auth/auth-router.js');
@@ -13,7 +13,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
+server.use('/api/auth', usernameAvailability, authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
 
 module.exports = server;
